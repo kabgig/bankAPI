87f3fea5e320f4130b1c2d2a87fc7eebfc0705be
Ansar Ismagilov <ansar89kab@bk.ru>
2023-12-03 18:07:06 +0800

    successful launch

---
+++ bankApi/src/main/java/com/kabgig/bankApi/service/UserBalanceService.java
@@ -0,0 +1,22 @@
+package com.kabgig.bankApi.service;
+
+import com.kabgig.bankApi.repository.UserBalanceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserBalanceService {
+
+    @Autowired
+    private UserBalanceRepository userBalanceRepository;
+
+    public void getBalance(){
+
+    }
+    public void putMoneу(){
+
+    }
+    public void takeMoney(){
+
+    }
+}
---
+++ bankApi/src/main/java/com/kabgig/bankApi/utils/DatabaseConfig.java
@@ -0,0 +1,9 @@
+package com.kabgig.bankApi.utils;
+
+public class DatabaseConfig {
+    public static final String DATABASE_URL = "jdbc:postgresql://localhost:5432/bankApi";
+    public static final String DATABASE_USER = "root";
+    public static final String DATABASE_PASSWORD = "1234";
+    public static final String DATABASE_DRIVER = "org.postgresql.Driver";
+    public static final String HIBERNATE_DIALECT = "org.hibernate.dialect.PostgreSQLDialect";
+}
